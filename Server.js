f5333d4a8a9ac7c899678bcf3745c9df7b3cecd8
brenwick <brenwick02@gmail.com>
2020-10-25 12:44:54 -0700

    reworked routes

--- Server.js
+++ Server.js
@@ -1,6 +1,5 @@
 //jshint esversion:6
 const express = require("express");
-const path  = require("path");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 // No variable assignment since User is not exported
@@ -36,15 +35,17 @@ mongoose.connection.on('error', (err) => {
 
 
 //-----------------------------------------------Express Server---------------------------------------------------------------
-app.listen(PORT, function() {
+app.listen(process.env.PORT || PORT, function() {
   console.log("Server started on port: " + PORT);
 });
 
-// TODO -- TRY / CATCH Blocks!!!
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+})
 
 // GET All Users
-app.get('/users', async (req, res) => {
+app.get('/get_all_users', async (req, res) => {
   console.log('All Users Requested from API.');
   const filter = {};
   const all = await User.find(filter);
@@ -52,71 +53,112 @@ app.get('/users', async (req, res) => {
 });
 
 // Get User by Id
-app.get('/userId:id', async (req, res) => {
-  console.log('User Requested by ID: ' + req.params.id);
-  const user = await User.findById(mongoose.Types.ObjectId(req.params.id))
-  res.send(user);
+app.get('/get_user_by_id', async (req, res) => {
+  if(req.body.id) {
+    try{
+      const user = await User.findById(mongoose.Types.ObjectId(req.body.id));
+      res.send(user);
+    }
+    catch(error){
+      res.send('Error: User not found.')
+    }
+  }
+  else {
+    res.send('Error: please send the MongoDB user ID in JSON format.')
+  }
 })
 
-// Get User by username
-app.get('/userName:name', async (req, res) => {
-  console.log('User Requested by username: ' + req.params.name);
-  const user = await User.findOne({ username: req.params.name });
-  res.send(user);
-})
 
 // CREATE NEW User profile
-app.post('/user', (req, res) => {
+app.post('/create_user', (req, res) => {
+  console.log('\nAttempting to Add User.');
   console.log(req.body);
-  const newUser = new User({
-    username: req.body.username,
-    email: req.body.email,
-    phone: req.body.phone,
-    password: req.body.password,
-    retypePassword: req.body.retypePassword
-  });
-  newUser.save()
-  .then(data => {
-      console.log(data);
-      res.json({
-        "message" : "Successfully added user to database"
-      })
-    }).catch(err => {
-      console.log(err);
-      res.json({
-        "error" : "server error"
+  // JSON Information Check
+  if(req.body.username && req.body.email && req.body.phone && req.body.password && req.body.retypePassword) {
+    //Password match check
+    if(req.body.password === req.body.retypePassword) {
+      const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        phone: req.body.phone,
+        password: req.body.password,
+        retypePassword: req.body.retypePassword
       });
-  });
+      newUser.save()
+      .then(data => {
+        res.json({
+          "message" : "Successfully added user to database"
+        })
+      }).catch(err => {
+        console.log(err);
+        res.json({
+          "error" : "server error"
+        });
+    });
+    }
+    else{
+      res.send('Error: Passwords did not match')
+    }
+  }
+  else {
+    res.send('Error: please include username, email, phone, password, and retype-password in JSON format');
+  }
 });
 
 
 // DELETE user by name 
 //TODO: This is very unsecure and should only be used for debugging
-app.post('/deleteuser', async (req, res) => {
-  await User.findByIdAndDelete(req.body.payload._id)
-  .then(
-    console.log(req.body),
-    console.log('Deleted User: ' + req.body.payload._id),
-    res.send(await User.find({}))
-  )
-  .catch(err => {
-    console.log(err);
-  })
-})
+// app.post('/deleteuser', async (req, res) => {
+//   await User.findByIdAndDelete(req.body.payload._id)
+//   .then(
+//     console.log(req.body),
+//     console.log('Deleted User: ' + req.body.payload._id),
+//     res.send(await User.find({}))
+//   )
+//   .catch(err => {
+//     console.log(err);
+//   })
+// })
 
 
 // UPDATE existing user record
-app.post('/updateuser', (req, res)=> {
-  User.findByIdAndUpdate(req.body.userId, {
-    username: req.body.username,
-    email: req.body.email,
-    phone: req.body.phone,
-    password: req.body.password
-  })
-  .then(res.send({"message" : "Updated user: " + req.body.username}))
-  .catch(err => {
-    console.log(err);
-  })
-})
+// app.post('/updateuser', (req, res)=> {
+//   User.findByIdAndUpdate(req.body.userId, {
+//     username: req.body.username,
+//     email: req.body.email,
+//     phone: req.body.phone,
+//     password: req.body.password
+//   })
+//   .then(res.send({"message" : "Updated user: " + req.body.username}))
+//   .catch(err => {
+//     console.log(err);
+//   })
+// });
 
+
+
+// Obtain JSON Web Token
+app.get('/login', async (req, res) => {
+
+  if(req.body.password && req.body.id) {
+    password = req.body.password;
+    id = req.body.id;
+    try{
+      const user = await User.findById(id);
+      if(password === user.password) {
+        res.send('User Logged In');
+      }
+      else {
+        res.send('Error: Password did not match')
+      }
+    }
+    catch(error){
+      res.send('Error: username or password did not match');
+      console.log(error);
+    } 
+  }
+  else {
+    res.send('Error: Please include user id and password in request body');
+  }
+});
 
